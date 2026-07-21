9f2cf570756ca6382c2622d12b27875aaa1b87a9
Adrian Pasik <adrian.pasik@vp.pl>
2026-07-21 14:24:01 +0200

    first module

--- src/app/frontend/language-select/language-select.ts
+++ src/app/frontend/language-select/language-select.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-language-select',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './language-select.html',
   styleUrl: './language-select.css',
 })
 export class LanguageSelect {
 
+  options: ComboBoxOption[] = [
+    { value: 'plde', label: 'PL -> DE' }
+  ];
+  selectedOption = signal('');
+
+
+  onSelectionChange(event: Event): void {
+    const selectElement = event.target as HTMLSelectElement;
+    const selectedValue = selectElement.value;
+    this.selectedOption.set(selectedValue);
+  }
+}
+
+interface ComboBoxOption {
+  value: string;
+  label: string;
 }
