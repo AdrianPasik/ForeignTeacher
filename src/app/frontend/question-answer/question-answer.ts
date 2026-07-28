f84ec2be80a9b86831a5474f8f8848cfbac33e05
Adrian Pasik <adrian.pasik@vp.pl>
2026-07-28 22:09:51 +0200

    question answer scaffold

--- src/app/frontend/question-answer/question-answer.ts
+++ src/app/frontend/question-answer/question-answer.ts
@@ -1,14 +1,32 @@
-import { Component, input } from '@angular/core';
+import { Component, Input, input, Signal, signal } from '@angular/core';
 import { Progress } from '../../services/persistence/progress';
 import { Learning } from '../../services/learning';
+import { DisplayResult } from "../display-result/display-result";
 
 @Component({
-  selector: 'app-question-answer',
-  imports: [],
-  templateUrl: './question-answer.html',
-  styleUrl: './question-answer.css',
+    selector: 'app-question-answer',
+    imports: [DisplayResult],
+    templateUrl: './question-answer.html',
+    styleUrl: './question-answer.css',
 })
 export class QuestionAnswer {
-  selectedLanguage = input.required<string>();
-  //learning = input.required<Learning>();
+    selectedLanguage = input.required<string>();
+    learningService = input.required<Learning | null>();
+    knownLanguageText = signal("");
+    userTranslation = signal("");
+    checkAnswerCaption = signal("Check");
+
+    onInputChange(event: any): void {
+        this.userTranslation.set(event.target.value);
+    }
+
+    onButtonClick(): void {
+        if (this.learningService() == null) {
+            console.error('Learning service was not injected properly');
+        }
+        const realService = <Learning>this.learningService();
+
+        // Empty logic for now
+        console.log('Button clicked');
+    }
 }
