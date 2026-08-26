6e17cedea6f04ff84b5e4e44bff2069cbabf321d
Adrian Pasik <adrian.pasik@vp.pl>
2026-08-26 12:34:01 +0200

    layout changes

--- src/app/frontend/display-result/display-result.ts
+++ src/app/frontend/display-result/display-result.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { Progress } from '../../services/persistence/progress';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector: 'app-display-result',
-    imports: [CommonModule],
+    imports: [CommonModule, MatIconModule],
     templateUrl: './display-result.html',
     styleUrl: './display-result.css',
 })
